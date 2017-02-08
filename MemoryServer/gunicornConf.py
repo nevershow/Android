27076079791dff1b1c6e8dd719fcc7b49ccc369f
nevershow <719721165@qq.com>
2017-02-08 19:52:30 +0800

    Memory server side

--- MemoryServer/gunicornConf.py
+++ MemoryServer/gunicornConf.py
@@ -1,12 +1,15 @@
-import os
+# *-* coding: utf-8 *-*
 import gevent.monkey
 import multiprocessing
 
+reload = True         # 修改代码时自动重启gunicorn
+bind = "0.0.0.0:23333" # 绑定ip和端口
 gevent.monkey.patch_all()
-debug = True
-loglevel = "debug"
-bind = "0.0.0.0:23333"
 workers = multiprocessing.cpu_count() * 2
-worker_class = "gunicorn.workers.ggevent.GeventWorker"
-keepalive = 4
-threads = 2
+worker_class = "gevent"
+keepalive = 5
+threads = multiprocessing.cpu_count() * 2
+backlog = 2048
+proc_name = 'gunicorn.proc'
+pidfile = '/tmp/gunicorn.pid'
+debug = True
